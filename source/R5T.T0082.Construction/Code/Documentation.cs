047cd45e3bc5b0ee28d209c44d11998d14b982f6
davidcoats <david@getsafetycone.com>
2021-11-09 10:12:07 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0082.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0082.Construction
+{
+    /// <summary>
+    /// Construction project for IConfigurationSource marker interface and attribute for identifying configuration source types. .
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0082/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0082
+{
+    /// <summary>
+    /// IConfigurationSource marker interface and attribute for identifying configuration source types. 
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
